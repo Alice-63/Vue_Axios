9a2e5805aeaae12b681c08c7fb7bd16e8ebfd4f3
Alice-63 <alicetindag84@gmail.com>
2021-11-08 21:25:51 +0100

    first commit

---
+++ src/components/Posts.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="container-fluid pt-5">
+    <h1 class="text-center">Yazı Listesi</h1>
+    <hr>
+    <div class="d-flex flex-wrap flex-row justify-content-center align-items-center">
+      <div class="card m-1">
+        <img class="card-img-top" src="https://i.ytimg.com/vi/a1kDpfwO16s/maxresdefault.jpg" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+            content.</p>
+        </div>
+      </div>
+      <div class="card m-1">
+        <img class="card-img-top" src="https://i.ytimg.com/vi/a1kDpfwO16s/maxresdefault.jpg" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+            content.</p>
+        </div>
+      </div>
+      <div class="card m-1">
+        <img class="card-img-top" src="https://i.ytimg.com/vi/a1kDpfwO16s/maxresdefault.jpg" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+            content.</p>
+        </div>
+      </div>
+      <div class="card m-1">
+        <img class="card-img-top" src="https://i.ytimg.com/vi/a1kDpfwO16s/maxresdefault.jpg" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+            content.</p>
+        </div>
+      </div>
+      <div class="card m-1">
+        <img class="card-img-top" src="https://i.ytimg.com/vi/a1kDpfwO16s/maxresdefault.jpg" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+            content.</p>
+        </div>
+      </div>
+      <div class="card m-1">
+        <img class="card-img-top" src="https://i.ytimg.com/vi/a1kDpfwO16s/maxresdefault.jpg" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+            content.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<style>
+  .card {
+    width: 300px;
+  }
+</style>
